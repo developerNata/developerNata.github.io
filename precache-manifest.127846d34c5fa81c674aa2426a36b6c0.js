3afaa2e3c71cf6705a60da734ffd22224cb42caf
Developer <developer@Developers-MacBook-Pro.local>
2022-09-26 22:51:42 +0300

    first commit

---
+++ precache-manifest.127846d34c5fa81c674aa2426a36b6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f56e22091743fc74e5561cf71fbed48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db6fd22c9621862e316",
+    "url": "/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "ed8f5125d2c43d8b8b5f",
+    "url": "/static/css/main.aad5db13.chunk.css"
+  },
+  {
+    "revision": "0db6fd22c9621862e316",
+    "url": "/static/js/2.17bd7dd3.chunk.js"
+  },
+  {
+    "revision": "ed8f5125d2c43d8b8b5f",
+    "url": "/static/js/main.ad53c34b.chunk.js"
+  },
+  {
+    "revision": "ac2aa59e6cac7255306d",
+    "url": "/static/js/runtime-main.7af02c87.js"
+  }
+]);
